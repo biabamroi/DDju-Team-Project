e9cc9130a2f793e1c4d8cea00f1f2d94ac559b84
biabamroi@gmail.com <biabamroi@gmail.com>
2023-08-10 22:20:29 +0900

    🔥 console.log 삭제

--- Project_vr2/JS/header.js
+++ Project_vr2/JS/header.js
@@ -1,7 +1,6 @@
 // .gnb-item hover시 sub-menu slide up & down
 $('.gnb-item').hover(function(){
   $(this).find('.sub').stop().slideDown(500);
-  console.log(this)
 }, function(){
   $(this).find('.sub').stop().slideUp(500);
 })
