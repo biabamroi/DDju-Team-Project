79bfbbb7f66c12866d5abb47d4dd4e2f81c8f751
jieun0212 <wldnjs4983@naver.com>
2023-08-23 16:53:26 +0900

    🚧 contact page 구현중

--- Project_vr2/JS/contact.js
+++ Project_vr2/JS/contact.js
@@ -5,8 +5,8 @@ let profile = [
     txt : 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Ut pariatur fugit saepe possimus accusamus facere? RepellendusUt pariatur fugit saepe possimus accusamus facere? RepellendusUt pariatur fugit saepe possimus accusamus facere? RepellendusUt pariatur fugit saepe possimus accusamus facere? RepellendusUt pariatur fugit saepe possimus accusamus facere? RepellendusUt pariatur fugit saepe possimus accusamus facere? RepellendusUt pariatur fugit saepe possimus accusamus facere? RepellendusUt pariatur fugit saepe possimus accusamus facere? RepellendusUt pariatur fugit saepe possimus accusamus facere? RepellendusUt pariatur fugit saepe possimus accusamus facere? RepellendusUt pariatur fugit saepe possimus accusamus facere? RepellendusUt pariatur fugit saepe possimus accusamus facere? RepellendusUt pariatur fugit saepe possimus accusamus facere? Repellendus deleniti saepe eum ipsum explicabo porro. Laborum rerum maxime odio, blanditiis molestias aliquam labore.',
     git : 'https://github.com/choiseowu',
     email : 'chldjtn95@nate.com',
-    color : 'rgb(154, 214, 255)',
-    shadow : 'rgba(142, 174, 243, 0.8)'
+    // color : 'rgb(154, 214, 255)',
+    // shadow : 'rgba(142, 174, 243, 0.8)'
   },
   {
     img : 'https://avatars.githubusercontent.com/u/132882429?v=4',
@@ -14,8 +14,8 @@ let profile = [
     txt : 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Ut pariatur fugit saepe possimus accusamus facere? RepellendusUt pariatur fugit saepe possimus accusamus facere? RepellendusUt pariatur fugit saepe possimus accusamus facere? RepellendusUt pariatur fugit saepe possimus accusamus facere? RepellendusUt pariatur fugit saepe possimus accusamus facere? RepellendusUt pariatur fugit saepe possimus accusamus facere? RepellendusUt pariatur fugit saepe possimus accusamus facere? RepellendusUt pariatur fugit saepe possimus accusamus facere? RepellendusUt pariatur fugit saepe possimus accusamus facere? RepellendusUt pariatur fugit saepe possimus accusamus facere? RepellendusUt pariatur fugit saepe possimus accusamus facere? RepellendusUt pariatur fugit saepe possimus accusamus facere? RepellendusUt pariatur fugit saepe possimus accusamus facere? Repellendus deleniti saepe eum ipsum explicabo porro. Laborum rerum maxime odio, blanditiis molestias aliquam labore.',
     git : 'https://github.com/Ji2unKo',
     email : 'wldnjs4983@naver.com',
-    color : 'rgb(252, 207, 214)',
-    shadow : 'rgba(243, 195, 203, 0.8)'
+    // color : 'rgb(252, 207, 214)',
+    // shadow : 'rgba(243, 195, 203, 0.8)'
   },
   {
     img : 'https://avatars.githubusercontent.com/u/132882278?v=4',
@@ -23,8 +23,8 @@ let profile = [
     txt : 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Ut pariatur fugit saepe possimus accusamus facere? RepellendusUt pariatur fugit saepe possimus accusamus facere? RepellendusUt pariatur fugit saepe possimus accusamus facere? RepellendusUt pariatur fugit saepe possimus accusamus facere? RepellendusUt pariatur fugit saepe possimus accusamus facere? RepellendusUt pariatur fugit saepe possimus accusamus facere? RepellendusUt pariatur fugit saepe possimus accusamus facere? RepellendusUt pariatur fugit saepe possimus accusamus facere? RepellendusUt pariatur fugit saepe possimus accusamus facere? RepellendusUt pariatur fugit saepe possimus accusamus facere? RepellendusUt pariatur fugit saepe possimus accusamus facere? RepellendusUt pariatur fugit saepe possimus accusamus facere? RepellendusUt pariatur fugit saepe possimus accusamus facere? Repellendus deleniti saepe eum ipsum explicabo porro. Laborum rerum maxime odio, blanditiis molestias aliquam labore.',
     git : 'https://github.com/hanna0115',
     email : 'hanna0115@naver.com',
-    color : 'rgb(139, 212, 123)',
-    shadow : 'rgba(162, 223, 148, 0.8)'
+    // color : 'rgb(139, 212, 123)',
+    // shadow : 'rgba(162, 223, 148, 0.8)'
   },
   {
     img : 'https://avatars.githubusercontent.com/u/132882475?v=4',
@@ -32,8 +32,8 @@ let profile = [
     txt : 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Ut pariatur fugit saepe possimus accusamus facere? RepellendusUt pariatur fugit saepe possimus accusamus facere? RepellendusUt pariatur fugit saepe possimus accusamus facere? RepellendusUt pariatur fugit saepe possimus accusamus facere? RepellendusUt pariatur fugit saepe possimus accusamus facere? RepellendusUt pariatur fugit saepe possimus accusamus facere? RepellendusUt pariatur fugit saepe possimus accusamus facere? RepellendusUt pariatur fugit saepe possimus accusamus facere? RepellendusUt pariatur fugit saepe possimus accusamus facere? RepellendusUt pariatur fugit saepe possimus accusamus facere? RepellendusUt pariatur fugit saepe possimus accusamus facere? RepellendusUt pariatur fugit saepe possimus accusamus facere? RepellendusUt pariatur fugit saepe possimus accusamus facere? Repellendus deleniti saepe eum ipsum explicabo porro. Laborum rerum maxime odio, blanditiis molestias aliquam labore.',
     git : 'https://github.com/biabamroi',
     email : 'biabamroi@gmail.com',
-    color : 'rgb(164, 130, 184)',
-    shadow : 'rgba(204, 167, 226, 0.8)'
+    // color : 'rgb(164, 130, 184)',
+    // shadow : 'rgba(204, 167, 226, 0.8)'
   },
 ]
 
@@ -45,13 +45,13 @@ othersBtn.forEach(function(btn, index){
       item.classList.remove('on');
     })
     btn.classList.add('on');
-    document.querySelector('.wrapper').style.backgroundColor = profile[index].color
-    document.querySelector('.first').style.display = 'none'
+    // document.querySelector('.wrapper').style.backgroundColor = profile[index].color
+    document.querySelector('.first h1').style.display = 'none'
     document.querySelector('.profile-wrap').style.display = 'block'
 
     document.querySelector('.profile img').src = profile[index].img;
     document.querySelector('.profile img').alt = profile[index].title;
-    document.querySelector('.profile img').style.boxShadow = `5px 15px 30px ${profile[index].shadow}`
+    // document.querySelector('.profile img').style.boxShadow = `5px 15px 30px ${profile[index].shadow}`
     document.querySelector('.profile h1').innerHTML = profile[index].name;
     document.querySelector('.txt p').innerHTML = profile[index].txt;
 
@@ -59,4 +59,11 @@ othersBtn.forEach(function(btn, index){
     liTag[0].querySelector('a').href = profile[index].git;
     liTag[1].querySelector('span').innerHTML = profile[index].email;
   })
+})
+
+
+
+document.querySelector('.others').addEventListener('click', function(){
+  this.classList.add('active');
+  // document.querySelector('video').style.display = 'none'
 })
