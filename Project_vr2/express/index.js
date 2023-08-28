97e11a29887ccbde3935b28109fbaab5b0ded27b
Kai <biabamroi@gmail.com>
2023-08-28 15:16:27 +0900

    🚧 kakao api code 임시 저장

--- Project_vr2/express/index.js
+++ Project_vr2/express/index.js
@@ -1,6 +1,16 @@
 const express = require('express');
 const app = express();
+const path = require('path');
 
-app.listen(7000, function(){
-
+app.listen(3000, function(){
+  console.log('Server listening on port 3000');
 })
+
+app.get('/index', function(requests, response){
+  response.sendFile(__dirname + '/index.html');
+})
+
+app.get('/map', function(requests, response){
+  response.sendFile(__dirname + '/map.html');
+})
+
