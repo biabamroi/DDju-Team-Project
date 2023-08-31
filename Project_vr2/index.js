414d10f13c594d7451bd4cb9e62a8b0fb88489ec
Kai <biabamroi@gmail.com>
2023-08-31 13:30:02 +0900

    🚧 DB 작업 중

--- Project_vr2/index.js
+++ Project_vr2/index.js
@@ -25,6 +25,29 @@ app.use(express.static(__dirname));
 // 콜렉션 user (회원)
 // 콜렉션 zzim (좋아요) 
 
+// Database ID admin PW zbJIiHYEKSsLa6Jg
+MongoClient.connect('mongodb+srv://admin:zbJIiHYEKSsLa6Jg@data.faox2rv.mongodb.net/?retryWrites=true&w=majority', function(error, client){
+  if(error){
+    return console.log(error);
+  }
+  
+  db = client.db('DDju');
+  app.listen('3000', function(){
+    console.log('success');
+  });
+})
+
+
+// 회원가입, 로그인 수정 중 --------------------------------------------------------------------
+app.post('/user', function(requests, response){
+  response.send('전송완료!')
+  console.log(requests.body)
+
+  // 내 콜렉션 이름 / insertOne 오브젝트 값 중괄호 db에 넘겨줄 값, 콜백함수 (error, 결과값 받아볼 변수)
+  db.collection('user').insertOne({아이디 : requests.body.id, 비밀번호 : requests.body.pw}, function(error, result){
+    console.log('db저장완료');
+  })
+})
 
 // 기본 홈페이지 첫 화면
 app.get('/', function(requests, response){
