fff28196f5d1f00ce18963afe7b24e8679c6f255
Kai <biabamroi@gmail.com>
2023-09-05 14:51:48 +0900

    🚧 data, session 작업중

--- Project_vr2/index.js
+++ Project_vr2/index.js
@@ -5,43 +5,15 @@ const path = require('path');
 // 데이터를 저장할 변수
 let db;
 
-// 포트 3000 연결
-// app.listen(3000, function(){
-//   console.log('Server listening on port 3000');
-// })
-
 // .ejs 사용 세팅
 app.set('view engine', 'ejs');
 
 // 모든 정적 파일 제공
 app.use(express.static(__dirname));
 
-
-// bodyParser 사용 선언
-const bodyParser = require('body-parser');
-app.use(bodyParser.urlencoded({extended : true}));
-
-// cookieParser
-// 인스톨 npm install cookie-parser --save
-const cookieParser = require('cookie-parser');
-app.use(cookieParser());
-
-app.get('/', function (req, res) {
-  // Cookies that have not been signed 서명되지 않은 쿠키
-  console.log('Cookies: ', req.cookies)
-
-  // Cookies that have been signed 서명된 쿠키
-  console.log('Signed Cookies: ', req.signedCookies)
-});
-
-// 구문
-// cookieParser(secret)
-//  서버 쿠키 설정
-// res.cookie(key, value, option)
-// option : expires 날짜 만료 기간
-// httpOnly true 시 쿠키 접근 불가
-// maxAge 초단위 만료 기간
-// secure HTTPS일 경우만 쿠키 전송
+// npm install method-override 라이브러리
+const methodOverride = require('method-override');
+app.use(methodOverride('_method'));
 
 
 
@@ -62,145 +34,62 @@ MongoClient.connect('mongodb+srv://admin:zbJIiHYEKSsLa6Jg@data.faox2rv.mongodb.n
   }
 
   db = client.db('DDju');
+  // 포트 3000 연결
   app.listen('3000');
 })
 
 
-// 회원가입 시 아이디 중복체크 ??
 
+// bodyParser 사용 선언
+const bodyParser = require('body-parser');
+app.use(bodyParser.urlencoded({extended : true}));
 
-// 회원가입 --------------------------------------------------------------------
-app.post('/join', function(requests, response){
-  db.collection('total').findOne({name:'dataLength'}, function(error, result){
-    console.log(result.totalData);
-    let totalDataLength = result.totalData;
+// cookieParser
+// npm install cookie-parser --save
+const cookieParser = require('cookie-parser');
+app.use(cookieParser());
 
-    db.collection('user').insertOne({
-      _id : totalDataLength+1, 
-      ID : requests.body.userid, 
-      PW : requests.body.userpw, 
-      name : requests.body.username,
-      birth : requests.body.year + requests.body.month + requests.body.date,
-      gender : requests.body.gender,
-      email : requests.body.usermail,
-      phone : requests.body.country + requests.body.phonenum + requests.body.veritext,
-      adress : requests.body.sample6_postcode + requests.body.sample6_address + requests.body.sample6_detailAddress + requests.body.sample6_extraAddress
-    }, function(error, result){
-      if(error){
-        return console.log(error);
-      }
-    })
+app.get('/', function (req, res) {
+  // Cookies that have not been signed 서명되지 않은 쿠키
+  console.log('Cookies: ', req.cookies)
 
-    db.collection('total').updateOne({name : 'dataLength'},
-    {$inc : {totalData:1}},
-    function(error, result){
-      if(error){
-        return console.log(error);
-      }
-    })
-  })
-  response.redirect("/login.html");
-})
+  // Cookies that have been signed 서명된 쿠키
+  console.log('Signed Cookies: ', req.signedCookies)
+});
 
+// 구문 ---------------------------수정할 부분------------------------
+// cookieParser(secret)
+//  서버 쿠키 설정
+// res.cookie(key, value, option)
+// option : expires 날짜 만료 기간
+// maxAge 초단위 만료 기간
 
-// 로그인 --------------------------------------------------------------------
 
 // 세션 설치
 // npm install -s express-session
+const session = require('express-session');
 
-// 세션 환경세팅
+// 세션 환경세팅 ---------------------------작업중--------------------
 app.use(
-  expressSession({
+  ({
     secret: "my key",
     resave: true,
     saveUninitialized: true
   })
 );
 
-const userID = req.body.userid || req.query.userid;
-const userPW = req.body.userpw || req.query.userpw;
-
-if (requests.session.user) {
-  // 세션에 유저가 존재한다면
-  response.redirect("/index.html");
-} else {
-  response.redirect("/login.html");
-}
-
-if (req.session.user) {
-  // 세션에 유저가 존재한다면
-  console.log("이미 로그인중입니다.");
-  res.writeHead(200, { "Content-Type": "text/html; charset=utf8" });
-  res.write("<h1> already Login</h1>");
-  res.write(`[ID] : ${userID} [PW] : ${userPW}`);
-  res.write('<a href="/process/example">예시로</a>');
-  res.end();
-} else {
-  req.session.user = {
-    id: userID,
-    pw: userPW,
-    name: username,
-    authorized: true,
-  };
-  res.writeHead(200, { "Content-Type": "text/html; charset=utf8" });
-  res.write("<h1>Login Success</h1>");
-  res.write(`[ID] : ${userID} [PW] : ${userPW}`);
-  res.write('<a href="/index">Move</a>');
-  res.end();
-}
-
-app.post('/login', function(requests, response){
-  db.collection('user').findOne({
-    ID : requests.body.userid, 
-    PW : requests.body.userpw 
-  }, (function(error, users){
-    if(error){
-      return console.log(error);
-    }
-    if(!users){
-      return response.redirect('/login.html');
-      // response.send("<script>alert('아이디와 비밀번호를 다시 한 번 확인해 주세요.');</script>");
-    }
-    // 로그인 세션 또는 쿠키, 토큰 유지 기능 구현 필요
-    // requests.
-    return response.redirect('/index.html');
-  }))
-})
-
-// 세션 로그인, 로그아웃 --------------------------------------------------------------------
-
-
-app.get("/logout", (req, res) => {
-  console.log("로그아웃");
-
-  if (req.session.user) {
-    console.log("로그아웃중입니다!");
-    req.session.destroy((err) => {
-      if (err) {
-        console.log("세션 삭제 에러가 발생했습니다.");
-        return;
-      }
-      console.log("세션이 삭제됐습니다.");
-      res.redirect("/login.html");
-    });
-  } else {
-    console.log("로그인이 필요합니다.");
-    res.redirect("/login.html");
-  }
-});
-
-const appServer = http.createServer(app);
-
-appServer.listen(app.get("port"), () => {
-  console.log(`${app.get("port")}에서 서버실행중.`);
-});
-
-
-
-
-// login 상태에서 zzim 값을 데이터베이스에서 받아서 -> zzim 페이지에서 꺼내오기
-
-
+app.use(session({
+  resave: false,
+  saveUninitialized: false,
+  secret: process.env.COOKIE_SECRET,
+  cookie: {
+    // httpOnly: true 쿠키 접근 불가
+    httpOnly: true,
+    // secure: HTTPS일 경우만 쿠키 전송
+    secure: false,
+  },
+}));
+// -------------------------------------------------------------------
 
 
 // 기본 홈페이지 첫 화면
@@ -257,5 +146,129 @@ app.get('/today-see', function(requests, response){
 app.get('/zzim', function(requests, response){
   response.sendFile(__dirname + '/zzim.html');
 })
+
+
+
+
+// 회원가입 시 아이디 중복체크 - 추후 업데이트
+
+
+// 회원가입 --------------------------------------------------------------------
+app.post('/join', function(requests, response){
+  db.collection('total').findOne({name:'dataLength'}, function(error, result){
+    console.log(result.totalData);
+    let totalDataLength = result.totalData;
+
+    db.collection('user').insertOne({
+      _id : totalDataLength+1, 
+      ID : requests.body.userid, 
+      PW : requests.body.userpw, 
+      name : requests.body.username,
+      birth : requests.body.year + requests.body.month + requests.body.date,
+      gender : requests.body.gender,
+      email : requests.body.usermail,
+      phone : requests.body.country + requests.body.phonenum + requests.body.veritext,
+      adress : requests.body.sample6_postcode + requests.body.sample6_address + requests.body.sample6_detailAddress + requests.body.sample6_extraAddress
+    }, function(error, result){
+      if(error){
+        return console.log(error);
+      }
+    })
+
+    db.collection('total').updateOne({name : 'dataLength'},
+    {$inc : {totalData:1}},
+    function(error, result){
+      if(error){
+        return console.log(error);
+      }
+    })
+  })
+  response.redirect("/login.html");
+})
+
+
+// 로그인 --------------------------------------------------------------------
+
+const userID = req.body.userid || req.query.userid;
+const userPW = req.body.userpw || req.query.userpw;
+
+if (requests.session.user) {
+  // 세션에 유저가 존재한다면
+  response.redirect("/index.html");
+} else {
+  response.redirect("/login.html");
+}
+
+if (req.session.user) {
+  // 세션에 유저가 존재한다면
+  console.log("이미 로그인중입니다.");
+  res.writeHead(200, { "Content-Type": "text/html; charset=utf8" });
+  res.write("<h1> already Login</h1>");
+  res.write(`[ID] : ${userID} [PW] : ${userPW}`);
+  res.write('<a href="/process/example">예시로</a>');
+  res.end();
+} else {
+  req.session.user = {
+    id: userID,
+    pw: userPW,
+    name: username,
+    authorized: true,
+  };
+  res.writeHead(200, { "Content-Type": "text/html; charset=utf8" });
+  res.write("<h1>Login Success</h1>");
+  res.write(`[ID] : ${userID} [PW] : ${userPW}`);
+  res.write('<a href="/index">Move</a>');
+  res.end();
+}
+
+app.post('/login', function(requests, response){
+  db.collection('user').findOne({
+    ID : requests.body.userid, 
+    PW : requests.body.userpw 
+  }, (function(error, users){
+    if(error){
+      return console.log(error);
+    }
+    if(!users){
+      return response.redirect('/login.html');
+      // response.send("<script>alert('아이디와 비밀번호를 다시 한 번 확인해 주세요.');</script>");
+    }
+    // 로그인 세션 또는 쿠키, 토큰 유지 기능 구현 필요
+    // requests.
+    return response.redirect('/index.html');
+  }))
+})
+
+// 세션 로그인, 로그아웃 --------------------------------------------------------------------
+
+app.get("/logout", (req, res) => {
+  console.log("로그아웃");
+
+  if (req.session.user) {
+    console.log("로그아웃중입니다!");
+    req.session.destroy((err) => {
+      if (err) {
+        console.log("세션 삭제 에러가 발생했습니다.");
+        return;
+      }
+      console.log("세션이 삭제됐습니다.");
+      res.redirect("/login.html");
+    });
+  } else {
+    console.log("로그인이 필요합니다.");
+    res.redirect("/login.html");
+  }
+});
+
+const appServer = http.createServer(app);
+
+appServer.listen(app.get("port"), () => {
+  console.log(`${app.get("port")}에서 서버실행중.`);
+});
+
+
+
+
+// login 상태에서 zzim 값을 데이터베이스에서 받아서 -> zzim 페이지에서 꺼내오기
 
 
