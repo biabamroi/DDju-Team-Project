3036c54f6ba0bbbd89e9de6e25da2d78cf6dbf12
biabamroi@gmail.com <biabamroi@gmail.com>
2023-09-11 00:47:44 +0900

    🚚 코드 이동

--- Project_vr2/JS/header.js
+++ Project_vr2/JS/header.js
@@ -6,3 +6,46 @@ $('.gnb-item').hover(function(){
   $(this).find('.sub').stop().slideUp(500);
   $(this).find('a').removeClass('on');
 })
+
+
+
+// 로그인 
+window.addEventListener('DOMContentLoaded', () => {
+  // 로그인 상태를 가져오는 비동기 함수 호출
+  getUserLoggedInStatus()
+    .then((userLoggedIn) => {
+      // 가져온 로그인 상태에 따라 버튼을 숨기거나 표시
+      // const mymenu = $('.mymenu');
+      // const loginBtn = $('.login-btn');
+
+      if (userLoggedIn) {
+        $('.user-menu-btn').addClass('login');
+        // mymenu.css('display', 'flex');
+        // loginBtn.css('display', 'none');
+      } else {
+        $('.user-menu-btn').removeClass('login');
+        // mymenu.css('display', 'none');
+        // loginBtn.css('display', 'block');
+      }
+    })
+    .catch((error) => {
+      console.error('로그인 상태를 가져오는 중에 오류 발생:', error);
+    });
+});
+
+// 서버로부터 로그인 상태를 가져오는 비동기 함수
+async function getUserLoggedInStatus() {
+  try {
+    const response = await fetch('/get-user-status'); // 서버에서 로그인 상태를 반환하는 엔드포인트
+    
+    if (!response.ok) {
+      // 서버에서 오류 응답을 받았을 경우 처리
+      throw new Error(`서버 응답 오류: ${response.status}`);
+    }
+
+    const data = await response.json();
+    return data.userLoggedIn; // 서버에서 반환한 로그인 상태를 반환
+  } catch (error) {
+    throw error;
+  }
+}
